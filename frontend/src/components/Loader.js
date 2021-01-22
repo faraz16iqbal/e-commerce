3e1d89b5d85d2efa6669dc1c513c4a85528561fa
Faraz <faraziqbal@hotmai.co.uk>
2021-01-23 02:40:46 +0530

    Updated loader and spinner

--- frontend/src/components/Loader.js
+++ frontend/src/components/Loader.js
@@ -6,7 +6,12 @@ const Loader = () => {
     <Spinner
       animation="border"
       role="status"
-      style={{ width: "100px", height: "100px", margin: "auto" }}
+      style={{
+        width: "100px",
+        height: "100px",
+        margin: "auto",
+        display: "block",
+      }}
     >
       <span className="sr-only">Loading</span>
     </Spinner>
