057e03f4dd7892429664daf62fc7397b78d007ca
Faraz <faraziqbal@hotmai.co.uk>
2021-01-23 13:46:34 +0530

    Added cart items to local storage

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -5,13 +5,25 @@ import {
   productDetailsReducer,
   productListReducer,
 } from "./reducers/productReducers";
+import { cartReducer } from "./reducers/cartReducers";
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
+  cart: cartReducer,
 });
 
-const initialState = {};
+//adding cart data
+
+const cartItemsFromStorage = localStorage.getItem("cartItems")
+  ? JSON.parse(localStorage.getItem("cartItems"))
+  : [];
+
+const initialState = {
+  cart: {
+    cartItems: cartItemsFromStorage,
+  },
+};
 
 const middleware = [thunk];
 
