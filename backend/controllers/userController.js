1fd75f1fbda04093640134a41bd193c140dc9087
Faraz <faraziqbal@hotmai.co.uk>
2021-01-24 02:39:14 +0530

    Added auth middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -5,6 +5,7 @@ import User from "../models/userModel.js";
 //@desc Authenticate your & get token
 //@route POST /api/users/login
 //@access Public
+
 const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
   const user = await User.findOne({ email });
@@ -22,4 +23,23 @@ const authUser = asyncHandler(async (req, res) => {
   }
 });
 
-export { authUser };
+//@desc Get user profile
+//@route POST /api/users/profile
+//@access Private
+
+const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
+});
+
+export { authUser, getUserProfile };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,8 @@
 import express from "express";
 const router = express.Router();
-import { authUser } from "../controllers/userController.js";
-
-router.route("/login").post(authUser);
+import { authUser, getUserProfile } from "../controllers/userController.js";
+import { protect } from "../middleware/authMiddleware.js";
+router.post("/login", authUser);
+router.route("/profile").get(protect, getUserProfile);
 
 export default router;
