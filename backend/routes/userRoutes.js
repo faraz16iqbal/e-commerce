c1aa63ff997afe81affe35ae5452d1cb6ba33db6
Faraz <faraziqbal@hotmai.co.uk>
2021-01-24 02:43:24 +0530

    Commit

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,6 +2,7 @@ import express from "express";
 const router = express.Router();
 import { authUser, getUserProfile } from "../controllers/userController.js";
 import { protect } from "../middleware/authMiddleware.js";
+
 router.post("/login", authUser);
 router.route("/profile").get(protect, getUserProfile);
 
