90e06847548af9c80b17e0af74c8b01285b4f97b
Faraz <faraziqbal@hotmail.co.uk>
2021-01-28 15:45:45 +0530

    Added payment screen

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -39,10 +39,16 @@ const shippingAddressFromStorage = localStorage.getItem("shippingAddress")
   ? JSON.parse(localStorage.getItem("shippingAddress"))
   : {};
 
+// adding payment method
+const paymentMethodFromStorage = localStorage.getItem("paymentMethod")
+  ? JSON.parse(localStorage.getItem("paymentMethod"))
+  : {};
+
 const initialState = {
   cart: {
     cartItems: cartItemsFromStorage,
     shippingAddress: shippingAddressFromStorage,
+    paymentMethod: paymentMethodFromStorage,
   },
   userLogin: { userInfo: userInfoFromStorage },
 };
